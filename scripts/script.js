1febcb8d2395c52f80788f52d2e813bdc9f13988
Jefferson Alves <jefff9910@outlook.com>
2021-11-14 19:40:32 -0300

    Primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+let header = new Vue({
+    el:"#header",
+    data: {
+        menu: [
+            { opcao: 'Home'},
+            { opcao: 'Promoções'}
+        ],
+        submenu: [
+            { opcao: 'About'},
+            { opcao: 'Jobos'},
+            { opcao: 'Contact'}
+        ]
+    }
+
+});
+
