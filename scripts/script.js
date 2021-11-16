3585fc05f43c2a656a921cad17f5fc9b0a6d525a
Jefferson Alves <jefff9910@outlook.com>
2021-11-15 21:59:05 -0300

    header e section em andamento

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,35 @@ let header = new Vue({
             { opcao: 'Promoções'}
         ],
         submenu: [
-            { opcao: 'About'},
-            { opcao: 'Jobos'},
-            { opcao: 'Contact'}
+            { opcao: 'Estados'},
+            { opcao: 'Cidades'},
+            { opcao: 'Histórias'}
         ]
     }
 
 });
 
+console.log(numero)
+let tituloMain = new Vue({
+    el:"#tituloMain",
+    data: {
+        title: "Desvendando o Brasil",
+        cont: numero,
+        contador_teste: 0,
+    },
+    created: {
+        exemplo: function() {
+            let tinterval = setInterval(()=>{
+                alert('Olaa');
+            }, 1000)
+            return tinterval
+        }
+    },
+    computed: {
+        changeTitle:function () {
+
+            return this.title.substring(0,this.cont)
+        }
+    }
+
+});
